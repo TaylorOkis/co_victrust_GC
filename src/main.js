f703f5af4692f3890ee851bdfba1595d173a133b
okistaylor <72969270+TaylorOkis@users.noreply.github.com>
2026-05-23 13:46:56 +0100

    Add basic setup

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
 import "./style.css";
 
-document.querySelector("#app").innerHTML = ``;
+document.querySelector("#app").innerHTML = `
+<header>This is the header</header>
+<main style="background-color: red;">This is the main</main>
+<footer>This is the Footer</footer>
+`;
